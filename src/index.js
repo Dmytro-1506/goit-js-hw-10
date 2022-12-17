4e084993b66eef8c53e777c9506f0e5ef8ec1b67
Dmytro-1506 <kyunlik@gmail.com>
2022-12-17 23:25:30 +0100

    preparing

--- src/index.js
+++ src/index.js
@@ -1,5 +1,36 @@
 import './css/styles.css';
-import lodash from 'lodash.debounce';
+import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
+const refs = {
+    input: document.querySelector('#search-box'),
+};
+
+refs.input.addEventListener('input', debounce(() => {
+    
+}, DEBOUNCE_DELAY));
+
+
+function fetchCountries(name) {
+
+//     https://restcountries.com/v2/{service}?fields={field},{field},{field}
+//     https://restcountries.com/v3.1/name/{name}
+//     name.official - полное имя страны
+    // capital - столица
+    // population - население
+    // flags.svg - ссылка на изображение флага
+    // languages - массив языков
+}
+
+function toManyMatches() {
+    Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
+}
+
+function errorName() {
+    Notiflix.Notify.failure("Oops, there is no country with that name");
+}
+
+toManyMatches();
+errorName();
+// Notiflix.Notify.info('Cogito ergo sum');
