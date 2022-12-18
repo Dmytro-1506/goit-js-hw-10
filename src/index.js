599c4edd0eed4af0ec98d6ade5261d6c7dab3cb4
Dmytro-1506 <kyunlik@gmail.com>
2022-12-18 23:47:23 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,32 +5,63 @@ import Notiflix from 'notiflix';
 const DEBOUNCE_DELAY = 300;
 const refs = {
     input: document.querySelector('#search-box'),
+    countryList: document.querySelector('.country-list'),
+    countryInfo: document.querySelector('.country-info'),
 };
+
+function createCountryList(object) {
+    let countriesArr = [];
+    Object.values(object).map(elem => {
+        const { name, flags } = elem;
+        countriesArr.push(`<svg class="flag-svg" width="70" height="70"><use href="${flags.svg}"></use></svg><span class="country-name">${name.official}</span>`)
+    });
+    refs.countryList.innerHTML = countriesArr.toString();
+}
 
 refs.input.addEventListener('input', debounce(() => {
     let countryName = refs.input.value.trim().toLowerCase();
-    console.log(countryName);
-    fetchCountries(countryName).then(
-        response => {
-            let countriesList = response.json();
-            console.log(countriesList);
-            return countriesList;
-        }
-    ).then(
-        response => {
-            return response.map(elem=>elem.name.common.toLowerCase())
-        }
-    ).then(
-        response => {
-            console.log(response);
-        })
+    getAllCountries(countryName);
 }, DEBOUNCE_DELAY));
 
+function getAllCountries(countryName) {
+    const allCountries = fetch('https://restcountries.com/v2/all?fields=name,capital,population,flags,languages');
+    allCountries.then(
+        response => {
+            return response.json();
+        }
+    ).then(
+        response => {
+            return response.map(elem => elem.name.toLowerCase());
+        }
+    ).then(
+        response => {
+            let checkedCountryNames = [];
+            response.map(elem => {
+                countryNameCheck(elem, countryName, checkedCountryNames);
+            });
+            let countriesQuantity = checkedCountryNames.length;
+            if (countriesQuantity > 10) {
+                toManyMatches();
+            } else if (countriesQuantity === 0) {
+                errorName();
+            } else {
+                return checkedCountryNames;
+            }
+        }
+    ).then(
+        response => {
+            let countriesList = [];
+            console.log(response);
+        }
+    ).catch(err => {
+            console.log(err);
+        })
+}
 
 function fetchCountries(name) {
-    const country = fetch('https://restcountries.com/v3.1/all');
-console.log(country, name);
-return country;
+    return fetch(`https://restcountries.com/v2/all?fields=name,capital,population,flags,languages`);
+    console.log(AllCountries);
+
 //     https://restcountries.com/v2/{service}?fields={field},{field},{field}
 //     https://restcountries.com/v3.1/name/{name}
 //     name.official - полное имя страны
@@ -48,6 +79,12 @@ function errorName() {
     Notiflix.Notify.failure("Oops, there is no country with that name");
 }
 
+function countryNameCheck(country, name, newArr) {
+    const chechedName = country.includes(name);
+    if (chechedName) {
+        newArr.push(country);
+    }
+}
+
 toManyMatches();
 errorName();
-fetchCountries(2).then(data => console.log(data.json()));
