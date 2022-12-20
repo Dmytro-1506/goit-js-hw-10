23eed943cf2b0577a2f9ba2f1ee8e8d42d8dd979
Dmytro-1506 <kyunlik@gmail.com>
2022-12-20 22:38:17 +0100

    task complete

---
+++ src/fetchCountries.js
@@ -0,0 +1,90 @@
+import Notiflix from 'notiflix';
+
+const refs = {
+    input: document.querySelector('#search-box'),
+    countryList: document.querySelector('.country-list'),
+    countryInfo: document.querySelector('.country-info'),
+};
+
+export default function fetchCountries(countryName) {
+    const allCountries = fetch('https://restcountries.com/v2/all?fields=name,capital,population,flags,languages');
+    allCountries.then(
+        response => {
+            return response.json();
+        }
+    ).then(
+        response => {
+            console.log(response);
+            let checkedCountryNames = [];
+            const namesList = response.map(elem => elem.name.toLowerCase());
+            namesList.map(elem => {
+                countryNameCheck(elem, countryName, checkedCountryNames);
+            });
+            const newNamesList = countriesQuantityCheck(checkedCountryNames);
+            const countriesToShow = getCountriesByNames(response, newNamesList);
+            console.log(countriesToShow);
+            if (countriesToShow.length > 1) {
+            let stringToHtml = [];
+            countriesToShow.forEach(country => {
+                createCountryItem(country, stringToHtml);
+            });
+                refs.countryInfo.innerHTML = stringToHtml.toString();
+            } else {
+                refs.countryInfo.innerHTML = createOneCountry(countriesToShow[0]);
+            }
+            
+        }
+    ).catch(err => {
+            console.log(err);
+        })
+}
+
+function createOneCountry(country) {
+    let leng = country.languages;
+    let arrLeng = [];
+    leng.map(elem => {
+        arrLeng.push(elem.name);
+    });
+    return `<div class="one-counrty"><img class="one-flag" src="${country.flags.svg}" width="160" height="80"></img><span class="country-name">${country.name}</span><p class="item-info">Capital: ${country.capital}</p><p class="item-info">Population: ${country.population}</p><p class="item-info">Languages: ${arrLeng.join(', ')}</p></div>`
+}
+
+function createCountryItem(country, arrey) {
+    arrey.push(`<div class="counrty-item"><img class="flag-svg" src="${country.flags.svg}" width="100" height="50"></img><span class="country-name">${country.name}</span></div>`);
+}
+
+function getCountriesByNames(allCountries, newNamesList) {
+    let newCountriesList = [];
+    newNamesList.map(name => {
+        allCountries.forEach(object => {
+            if (object.name.toLowerCase().includes(name)) {
+                newCountriesList.push(object);
+            }
+        })
+    })
+    return newCountriesList;
+}
+
+function countriesQuantityCheck(arrey) {
+    if (arrey.length > 10) {
+        toManyMatches();
+    } else if (arrey.length === 0) {
+        errorName();
+    } else {
+        return arrey;
+    }
+}
+
+function toManyMatches() {
+    Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
+}
+
+function errorName() {
+    Notiflix.Notify.failure("Oops, there is no country with that name");
+}
+
+function countryNameCheck(country, name, newArr) {
+    const chechedName = country.includes(name);
+    if (chechedName) {
+        newArr.push(country);
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
-import Notiflix from 'notiflix';
+import fetchCountries from './fetchCountries.js'
 
 const DEBOUNCE_DELAY = 300;
 const refs = {
@@ -9,82 +9,7 @@ const refs = {
     countryInfo: document.querySelector('.country-info'),
 };
 
-function createCountryItem(country, arrey) {
-    arrey.push(`<div class="counrty-item"><img class="flag-svg" src="${country.flags.svg}" width="100" height="50"></img><span class="country-name">${country.name}</span></div>`);
-}
-
 refs.input.addEventListener('input', debounce(() => {
     let countryName = refs.input.value.trim().toLowerCase();
-    getAllCountries(countryName);
+    fetchCountries(countryName);
 }, DEBOUNCE_DELAY));
-
-function getAllCountries(countryName) {
-    const allCountries = fetch('https://restcountries.com/v2/all?fields=name,capital,population,flags,languages');
-    allCountries.then(
-        response => {
-            return response.json();
-        }
-    ).then(
-        response => {
-            console.log(response);
-            let checkedCountryNames = [];
-            const namesList = response.map(elem => elem.name.toLowerCase());
-            namesList.map(elem => {
-                countryNameCheck(elem, countryName, checkedCountryNames);
-            });
-            const newNamesList = countriesQuantityCheck(checkedCountryNames);
-            const countriesToShow = getCountriesByNames(response, newNamesList);
-            let stringToHtml = [];
-            countriesToShow.forEach(country => {
-                createCountryItem(country, stringToHtml);
-            });
-            refs.countryInfo.innerHTML = stringToHtml.toString();
-        }
-    ).catch(err => {
-            console.log(err);
-        })
-}
-
-function getCountriesByNames(allCountries, newNamesList) {
-    let newCountriesList = [];
-    newNamesList.map(name => {
-        allCountries.forEach(object => {
-            if (object.name.toLowerCase().includes(name)) {
-                newCountriesList.push(object);
-            }
-        })
-    })
-    return newCountriesList;
-}
-
-function countriesQuantityCheck(arrey) {
-    if (arrey.length > 10) {
-        toManyMatches();
-    } else if (arrey.length === 0) {
-        errorName();
-    } else {
-        return arrey;
-    }
-}
-
-function fetchCountries(name) {
-    return fetch(`https://restcountries.com/v2/all?fields=name,capital,population,flags,languages`);
-}
-
-function toManyMatches() {
-    Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
-}
-
-function errorName() {
-    Notiflix.Notify.failure("Oops, there is no country with that name");
-}
-
-function countryNameCheck(country, name, newArr) {
-    const chechedName = country.includes(name);
-    if (chechedName) {
-        newArr.push(country);
-    }
-}
-
-toManyMatches();
-errorName();
